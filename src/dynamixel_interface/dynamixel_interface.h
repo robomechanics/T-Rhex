7a359741dc94a5b63d82eca4b0b4ef215a9c5ed5
Vivaan Bahl <vivaanbahl@gmail.com>
2019-04-15 19:38:22 -0400

    finished dynamixel interface header

--- src/dynamixel_interface/dynamixel_interface.h
+++ src/dynamixel_interface/dynamixel_interface.h
@@ -8,6 +8,12 @@
 #ifndef DYN_INTF_H
 #define DYN_INTF_H
 
+#include <cstdint>
+
+#include "dynamixel_sdk.h"
+#include "config/config.h"
+#include "instruction_parser/instruction_parser.h"
+
 enum DynInterfaceState
 {
     INIT,
@@ -23,8 +29,27 @@ enum DynInterfaceState
 
 class DynamixelInterface
 {
+public:
+    bool get_cmd_finished();
+    int8_t set_set_baud_rate(bool set_baud_rate);
+    int8_t set_desired_baud(uint32_t desired_baud);
+    int8_t set_instr(Instruction instr);
+    uint16_t *get_pos_data();
+    bool *get_finished();
+    int8_t set_shutdown(bool shutdown);
+
 private:
     DynInterfaceState current_state;
+
+    dynamixel::PortHandler port_handler;
+    dynamixel::PacketHandler adapter;
+    bool cmd_finished;
+    bool set_baud_rate;
+    uint32_t desired_baud;
+    Instruction instr;
+    uint16_t pos_data[NUM_DYNAMIXELS] pos_data;
+    bool finished[NUM_DYNAMIXELS];
+    bool shutdown;
 };
 
 #endif
