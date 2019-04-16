b2552f35ab86991571d8151638ada560c89779af
Vivaan Bahl <vivaanbahl@gmail.com>
2019-04-15 23:19:53 -0400

    added validator implementation

--- src/validator/validator.h
+++ src/validator/validator.h
@@ -10,6 +10,7 @@
 
 #include <cstdint>
 #include "instruction_parser/instruction_parser.h"
+#include "config/config.h"
 
 enum ValidatorState
 {
@@ -25,12 +26,23 @@ public:
     int8_t set_instruction(Instruction *instruction);
     int8_t set_eval_instruction(bool eval_instruction);
 
+    void tick();
+    Validator();
+
 private:
     ValidatorState current_state;
 
     bool is_valid;
-    Instruction instruction;
+    Instruction *instruction;
     bool eval_instruction;
+
+    int8_t errcode;
+    const uint16_t POS_LOWER_BOUND = 0;
+    const uint16_t POS_UPPER_BOUND = 4096;
+    const uint16_t VEL_LOWER_BOUND = 0;
+    const uint16_t VEL_UPPER_BOUND = 1023;
+
+    void evaluate_instruction();
 };
 
 #endif
