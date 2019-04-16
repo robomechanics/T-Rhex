36ffa8bfe23d720c3dd40688bbfae49a50593581
Vivaan Bahl <vivaanbahl@gmail.com>
2019-04-15 23:29:10 -0400

    committing so matt can use this computer

--- src/microcontroller/microcontroller.h
+++ src/microcontroller/microcontroller.h
@@ -9,6 +9,12 @@
 #ifndef MICRO_H
 #define MICRO_H
 
+#include <vector>
+
+#include "dynamixel_interface/dynamixel_interface.h"
+#include "instruction_parser/instruction_parser.h"
+#include "config/config.h"
+
 enum MicrocontrollerState
 {
     INIT,
@@ -22,8 +28,28 @@ enum MicrocontrollerState
 
 class Microcontroller
 {
+public:
+    int8_t set_reinit_cmd(bool reinitialize);
+    int8_t set_shutdown_cmd(bool shutdown);
+
 private:
     MicrocontrollerState current_state;
+
+    bool on_led;
+    DynamixelInterface dynlib;
+    uint16_t insctr;
+    Instruction initcmd;
+    bool reinitialize;
+    bool shutdown;
+    uint16_t leg_data[NUM_DYNAMIXELS];
+    Instruction curr_ins;
+    bool curr_ins_finished;
+    std::vector<Instruction> inst_set;
+    uint16_t N;
+    bool send_new_ins;
+    Instruction idle_cmd;
+    std::vector<Instruction> new_inst_set;
+    Instruction shutdown_cmd;
 };
 
 #endif
